c897d13edcc7f8f1aa6b7ddecf351f1bddfb2958
dashenka <darakudrakova@gmail.com>
2025-06-05 22:10:18 +0300

    add_body

--- nirs.py
+++ nirs.py
@@ -31,7 +31,17 @@ space.add(platform)
 
 
 def create_block(x, y):
-    pass
+    mass = 1
+    size = (BLOCK_SIZE, BLOCK_SIZE)
+    moment = pymunk.moment_for_box(mass, size)
+    body = pymunk.Body(mass, moment)
+    body.position = x, y
+    shape = pymunk.Poly.create_box(body, size)
+    shape.friction = 1.0
+    shape.elasticity = 0.0
+    shape.color = (244, 193, 193, 255)
+    space.add(body, shape)
+    return body
 
 # Q-learning агент
 class QAgent:
