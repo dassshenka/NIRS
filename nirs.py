56b2e137e240ff19fed28d18ad5b2184e45db4ea
dashenka <darakudrakova@gmail.com>
2025-06-05 22:33:11 +0300

    add_qagent

--- nirs.py
+++ nirs.py
@@ -66,15 +66,25 @@ class QAgent:
         return cx, cy
 
     def choose_action(self, state):
-            pass
+        if randint(0, 100) < int(self.epsilon * 100):
+            return randint(0, len(self.actions) - 1)
+        else:
+            self.ensure_state_exists(state)
+            return int(np.argmax(self.q_table[state]))
 
     def ensure_state_exists(self, state):
-        pass
+        if state not in self.q_table:
+            self.q_table[state] = np.zeros(len(self.actions))
 
     def learn(self, prev_state, action, reward, next_state):
-        pass
+        self.ensure_state_exists(prev_state)
+        self.ensure_state_exists(next_state)
+        predict = self.q_table[prev_state][action]  # оцениваем ошибку предсказания
+        target = reward + self.gamma * np.max(self.q_table[next_state])  # оцениваем правду (награда сейчаас + лучшая ожидаемая награда из нового состояния)
+        self.q_table[prev_state][action] += self.alpha * (target - predict)
+
     def decay_epsilon(self, factor=0.999, min_eps=0.05):
-        pass
+        self.epsilon = max(min_eps, self.epsilon * factor)
 
 agent = QAgent()
 
